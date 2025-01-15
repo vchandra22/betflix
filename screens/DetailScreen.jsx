2433d1fa8ce226e0a5ec457e7a29762781016f0e
vchandra22 <vincentchaandra@gmail.com>
2025-01-15 16:47:21 +0700

    feat: create carousel popular movies, horizontal list by genres, dll

--- screens/DetailScreen.jsx
+++ screens/DetailScreen.jsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity, Linking } from 'react-native';
 import axiosInstance from '../config/axiosInstance';
+import { WebView } from 'react-native-webview';
 
 const DetailScreen = ({ route }) => {
     const { movieId } = route.params;
     const [movie, setMovie] = useState(null);
+    const [videoUrl, setVideoUrl] = useState(null);
 
     useEffect(() => {
         const fetchMovie = async () => {
             try {
                 const response = await axiosInstance.get(`/movie/${movieId}`);
                 setMovie(response.data);
+
+                const videoResponse = await axiosInstance.get(`/movie/${movieId}/videos`);
+                const trailer = videoResponse.data.results.find(
+                    (video) => video.type === 'Trailer' && video.site === 'YouTube'
+                );
+                if (trailer) {
+                    setVideoUrl(`https://www.youtube.com/embed/${trailer.key}`);
+                }
             } catch (error) {
                 console.error(error);
             }
@@ -25,16 +35,31 @@ const DetailScreen = ({ route }) => {
         <View className="flex flex-1 bg-black pt-8">
             <Text className="text-white text-4xl font-bold m-4">Detail Film</Text>
             <ScrollView>
-                <Image
-                    source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
-                    className="h-96 w-full"
-                />
+                {videoUrl && (
+                    <WebView
+                        source={{ uri: videoUrl }}
+                        style={{ width: '100%', height: 250 }}
+                        javaScriptEnabled={true}
+                        domStorageEnabled={true}
+                    />
+                )}
                 <Text className="text-white text-3xl font-bold m-4">{movie.title}</Text>
                 <View className="flex flex-row gap-4 items-end justify-between w-full px-4">
                     <Text className="text-white text-start text-md">Popularity: {movie.popularity}</Text>
                     <Text className="text-white text-start text-md">{movie.original_language}</Text>
                 </View>
                 <Text className="text-white text-base m-4">{movie.overview}</Text>
+                
+                {videoUrl && (
+                    <TouchableOpacity
+                        onPress={() => {
+                            Linking.openURL(`https://www.youtube.com/watch?v=${videoUrl.split('/')[4]}`).catch(err => console.error('Failed to open URL', err));
+                        }}
+                        className="m-4 bg-red-600 p-4 rounded-full"
+                    >
+                        <Text className="text-white text-lg font-bold text-center">Tonton</Text>
+                    </TouchableOpacity>
+                )}
             </ScrollView>
         </View>
     );
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
+import {View, Text, FlatList, Image, TouchableOpacity, ScrollView, Dimensions} from 'react-native';
 import axiosInstance from '../config/axiosInstance';
 import { createStackNavigator } from '@react-navigation/stack';
-import DetailScreen from '../screens/DetailScreen'; // Pastikan Anda mengimpor DetailScreen
+import DetailScreen from '../screens/DetailScreen';
 
 const Stack = createStackNavigator();
+const { width: screenWidth } = Dimensions.get('window');
 
 const HomeScreen = ({ navigation }) => {
-    const [movies, setMovies] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const [moviesByGenre, setMoviesByGenre] = useState({});
+    const [mostViewed, setMostViewed] = useState([]);
 
     useEffect(() => {
         const fetchMovies = async () => {
             try {
-                const response = await axiosInstance.get('/movie/popular');
-                setMovies(response.data.results);
+
+                const genreResponse = await axiosInstance.get('/genre/movie/list');
+                setGenres(genreResponse.data.genres);
+
+                const movies = {};
+                await Promise.all(
+                    genreResponse.data.genres.map(async (genre) => {
+                        const movieResponse = await axiosInstance.get(`/discover/movie?with_genres=${genre.id}`);
+                        movies[genre.id] = movieResponse.data.results;
+                    })
+                );
+                setMoviesByGenre(movies);
+                
+                const mostViewedResponse = await axiosInstance.get('/movie/popular');
+                setMostViewed(mostViewedResponse.data.results.slice(0, 10));
             } catch (error) {
                 console.error(error);
             }
@@ -22,34 +38,79 @@ const HomeScreen = ({ navigation }) => {
         fetchMovies();
     }, []);
 
-    const renderItem = ({ item }) => (
+    const renderCarouselItem = ({ item }) => (
         <TouchableOpacity
-            className="mx-2 my-4 bg-gray-800 rounded-lg overflow-hidden"
+            className="mr-4 bg-gray-800 rounded-lg overflow-hidden"
+            onPress={() => navigation.navigate('Detail', { movieId: item.id })}
+        >
+            <Image
+                source={{ uri: `https://image.tmdb.org/t/p/w500${item.backdrop_path}` }}
+                style={{ width: screenWidth - 4, height: 400 }}
+                className="rounded-lg"
+            />
+            <View className="absolute bottom-4 left-4 right-4">
+                <Text className="text-white text-3xl font-bold">{item.title}</Text>
+                <Text className="text-gray-100 text-sm font-regular">{item.overview}</Text>
+            </View>
+        </TouchableOpacity>
+
+
+    );
+
+    const renderMovieItem = ({ item }) => (
+        <TouchableOpacity
+            className="mr-4 bg-gray-800 rounded-lg overflow-hidden"
             onPress={() => navigation.navigate('Detail', { movieId: item.id })}
         >
             <Image
                 source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
-                className="h-64 w-full"
+                className="h-56 w-full bg-cover"
             />
-            <View className="p-4">
-                <Text className="text-white text-start text-4xl mt-2">{item.title}</Text>
-                <View className="flex flex-row gap-4 items-end justify-between w-full">
-                    <Text className="text-white text-start text-md">Popularity: {item.popularity}</Text>
-                    <Text className="text-white text-start text-md">{item.original_language}</Text>
-                </View>
+            <View className="w-44 px-2 py-2">
+                <Text
+                    className="text-white text-start text-lg"
+                    numberOfLines={2}
+                    ellipsizeMode="tail"
+                >
+                    {item.title}
+                </Text>
             </View>
         </TouchableOpacity>
     );
 
+    const renderGenreSection = () => (
+        genres.map((genre) => (
+            <View key={genre.id} className="mb-6">
+                <Text className="text-white text-2xl font-bold mx-2 mb-2">{genre.name}</Text>
+                <FlatList
+                    horizontal
+                    data={moviesByGenre[genre.id] || []}
+                    renderItem={renderMovieItem}
+                    keyExtractor={(item) => item.id.toString()}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{ paddingHorizontal: 4 }}
+                />
+            </View>
+        ))
+    );
+
     return (
-        <View className="flex-1 bg-black pt-8">
+        <ScrollView className="flex-1 bg-black pt-8">
             <Text className="text-white text-4xl font-bold m-4">Film Terpopuler</Text>
             <FlatList
-                data={movies}
-                renderItem={renderItem}
+                data={mostViewed}
+                renderItem={renderCarouselItem}
                 keyExtractor={(item) => item.id.toString()}
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 0 }}
+                snapToAlignment="center"
+                decelerationRate="fast"
             />
-        </View>
+            
+            {renderGenreSection()}
+        </ScrollView>
     );
 };
 
